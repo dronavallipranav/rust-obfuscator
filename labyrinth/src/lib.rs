bbc43b1bab965acbbf613a4753ff87c53e0b8d63
Pranav Dronavalli <dronavallipranav@gmail.com>
2024-01-05 19:36:28 -0600

    add tests for encrypt_string logic

--- labyrinth/src/lib.rs
+++ labyrinth/src/lib.rs
@@ -30,3 +30,36 @@ fn xor_cipher(input: &str, key: &str) -> String {
         .map(|(input_char, key_char)| { ((input_char as u8) ^ (key_char as u8)) as char })
         .collect()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_xor_cipher_and_decrypt() {
+        let key = "xnasff3wcedj";
+        let test_strings = ["Hello", "World", "1234", "!@#$%^&*()"];
+
+        for &original in &test_strings {
+            let encrypted = xor_cipher(original, &key);
+            let decrypted = labyrinth_helper::decrypt_string(&encrypted, &key);
+            assert_eq!(original, decrypted, "Failed for string: {}", original);
+        }
+    }
+    #[test]
+    fn test_xor_cipher_and_decrypt_customkey() {
+        //set key
+        std::env::set_var("LABYRINTH_KEY", "testkey");
+        //test loc from encrypt_string meant to extract key
+        let key = env::var("LABYRINTH_KEY").unwrap_or_else(|_| "xnasff3wcedj".to_string());
+        assert_eq!(key, "testkey");
+
+        let test_strings = ["Hello", "World", "1234", "!@#$%^&*()"];
+
+        for &original in &test_strings {
+            let encrypted = xor_cipher(original, &key);
+            let decrypted = labyrinth_helper::decrypt_string(&encrypted, &key);
+            assert_eq!(original, decrypted, "Failed for string: {}", original);
+        }
+    }
+}
