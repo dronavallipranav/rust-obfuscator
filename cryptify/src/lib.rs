e8f5f443cfd46ee5b58c06b23d21a728aeaf7277
Pranav Dronavalli <dronavallipranav@gmail.com>
2024-01-09 19:58:27 -0600

    fixed decrypt_string return

--- cryptify/src/lib.rs
+++ cryptify/src/lib.rs
@@ -1,16 +1,19 @@
 //re-export proc macro crate
 pub use labyrinth_macros::*;
+use std::borrow::Cow;
 
 /// A helper decryption function meant to decrypt encrypted strings at runtime
 ///
 /// # Parameters
 /// - `input`: The encrypted string literal
 ///
-pub fn decrypt_string(encrypted: &str) -> String {
+pub fn decrypt_string(encrypted: &str) -> Cow<'static, str> {
     let key = std::env::var("CRYPTIFY_KEY").unwrap_or_else(|_| "xnasff3wcedj".to_string());
-    encrypted
+    let decrypted: String = encrypted
         .chars()
         .zip(key.chars().cycle())
         .map(|(encrypted_char, key_char)| ((encrypted_char as u8) ^ (key_char as u8)) as char)
-        .collect()
+        .collect();
+
+    Cow::Owned(decrypted)
 }
