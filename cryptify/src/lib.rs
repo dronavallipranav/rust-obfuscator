520155254b7950e3430b5bcf6ff6197b9c432da3
Pranav Dronavalli <dronavallipranav@gmail.com>
2024-01-09 22:05:08 -0600

    revert to String

--- cryptify/src/lib.rs
+++ cryptify/src/lib.rs
@@ -7,13 +7,11 @@ use std::borrow::Cow;
 /// # Parameters
 /// - `input`: The encrypted string literal
 ///
-pub fn decrypt_string(encrypted: &str) -> Cow<'static, str> {
+pub fn decrypt_string(encrypted: &str) -> String {
     let key = std::env::var("CRYPTIFY_KEY").unwrap_or_else(|_| "xnasff3wcedj".to_string());
-    let decrypted: String = encrypted
+    encrypted
         .chars()
         .zip(key.chars().cycle())
         .map(|(encrypted_char, key_char)| ((encrypted_char as u8) ^ (key_char as u8)) as char)
-        .collect();
-
-    Cow::Owned(decrypted)
+        .collect()
 }
